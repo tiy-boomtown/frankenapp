4389e8869cf8e6356dbb84367f985ada9a104759
James Dabbs <jamesdabbs@gmail.com>
2017-01-26 11:43:48 -0500

    Add parametrized steps for registering a user

--- features/step_definitions/devise_steps.rb
+++ features/step_definitions/devise_steps.rb
@@ -1,14 +1,14 @@
 Given(/^I do not have an account$/) do
   # no-op?
+  # User.where(email: 'james@example.com').delete_all
 end
 
 And(/^I'm on the sign up page$/) do
   visit '/users/sign_up'
 end
 
-When(/^I create a new account$/) do
-  # User.create!
-  fill_in 'user_email', with: 'james@example.com'
+When(/^I register as (\S+)$/) do |email|
+  fill_in 'user_email', with: email
   fill_in 'user_password', with: 'hunter2'
   fill_in 'user_password_confirmation', with: 'hunter2'
   click_on 'Sign up'
@@ -19,6 +19,35 @@ Then(/^I should be on the home page$/) do
   expect(page.has_content? 'Home Page!').to eq true
 end
 
-And(/^I should see my email$/) do
+And(/^I have a user account$/) do
+  # expect(User.where(email: 'james@example.com').count).to eq 1
   expect(page).to have_content 'james@example.com'
+end
+
+When(/^try mis\-matched passwords$/) do
+  fill_in 'user_email', with: 'james@example.com'
+  fill_in 'user_password', with: 'hunter2'
+  fill_in 'user_password_confirmation', with: 'hunterr2'
+  click_on 'Sign up'
+end
+
+Then(/^I should be on the sign up page$/) do
+  expect(page).to have_content 'Sign up'
+end
+
+And(/^I should see that passwords don't match$/) do
+  expect(page).to have_content "Password confirmation doesn't match Password"
+end
+
+Given(/^(\S+) has an account$/) do |email|
+  # step "register as #{email}"
+  User.create! email: email, password: 'hunter2'
+end
+
+And(/^I should see that the email is taken$/) do
+  expect(page).to have_content 'Email has already been taken'
+end
+
+And(/^there should be no new user accounts$/) do
+  expect(User.count).to eq 1
 end
--- features/user_auth.feature
+++ features/user_auth.feature
@@ -9,6 +9,14 @@ Feature: authenticating users
   Scenario: signing up for new account
     Given I do not have an account
       And I'm on the sign up page
-     When I create a new account
+     When I register as james@example.com
      Then I should be on the home page
-      And I should see my email
+      And I have a user account
+
+  Scenario: registering a taken email address
+    Given user@example.com has an account
+      And I'm on the sign up page
+     When I register as user@example.com
+     Then I should be on the sign up page
+      And I should see that the email is taken
+      And there should be no new user accounts
